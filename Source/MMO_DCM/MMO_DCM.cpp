12d319acb24e149db186721b364b0d87cd0b23ac
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-07-24 22:45:21 +0200

    HARD RESET

--- Source/MMO_DCM/MMO_DCM.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "MMO_DCM.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MMO_DCM, "MMO_DCM" );
